c373937a1132be5f61a728cb159e87bfdbfb5701
Sam <morgan.94.sam@gmail.com>
2020-10-16 15:45:00 +0100

    functionality complete

--- script.js
+++ script.js
@@ -7,74 +7,68 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-//Change Mode
-function changeMode(mode) {
-    const backgroundColor = '';
-    const onBackgroundColor = '';
-    const iconAdd = '';
-    const iconRemove = '';
-    const text = '';
-    
-    if (mode = 'dark'){
-        backgroundColor = '255 255 255';
-        onBackgroundColor = '0 0 0';
-        iconAdd = 'moon';
-        iconRemove = 'sun';
-        text = 'Dark';
+//declare var for attributes of theme other than name
+var modeTitle = '';
+var color1 = '';
+var color2 ='';
+var icon1 = '';
+var icon2 = '';
 
-    }else {
-        backgroundColor = '0 0 0';
-        onBackgroundColor = '255 255 255';
-        iconAdd = 'sun';
-        iconRemove = 'moon';
-        text = 'Light';
-    }
-
-    document.documentElement.setAttribute('data-theme', `${mode}`);
-    nav.style.backgroundColor = `rgb(${onBackgroundColor} / 50%)`;
-    textBox.style.backgroundColor = `rgb(${backgroundColor} / 50%)`;
-    toggleIcon.children[0].textContent = `${text} Mode`;
-    toggleIcon.children[1].classList.remove(`fa-${iconRemove}`);
-    toggleIcon.children[1].classList.add(`fa-${iconAdd}`);
-    image1.src = `img/undraw_proud_coder_${mode}.svg;`
-    image2.src = `img/undraw_feeling_proud_${mode}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
+//Dark or Light Images
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// //Dark Mode
-// function darkMode(){
-//     document.documentElement.setAttribute('data-theme', 'dark');
-//     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-//     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-//     toggleIcon.children[0].textContent = 'Dark Mode';
-//     toggleIcon.children[1].classList.remove('fa-sun');
-//     toggleIcon.children[1].classList.add('fa-moon');
-//     image1.src = 'img/undraw_proud_coder_dark.svg';
-//     image2.src = 'img/undraw_feeling_proud_dark.svg';
-//     image3.src = 'img/undraw_conceptual_idea_dark.svg';
-// }
+//Change attributes based on mode
+function changeMode(mode){
+    document.documentElement.setAttribute('data-theme', mode);
+    localStorage.setItem('theme',mode);
+    imageMode(mode);
 
-// //Light Mode
-// function lightMode(){
-//     document.documentElement.setAttribute('data-theme', 'light');
-//     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-//     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-//     toggleIcon.children[0].textContent = 'Light Mode';
-//     toggleIcon.children[1].classList.remove('fa-moon');
-//     toggleIcon.children[1].classList.add('fa-sun');
-//     image1.src = 'img/undraw_proud_coder_light.svg';
-//     image2.src = 'img/undraw_feeling_proud_light.svg';
-//     image3.src = 'img/undraw_conceptual_idea_light.svg';
-// }
+    if (mode === 'dark'){
+        modeTitle = 'Dark';
+        color1 = '0 0 0';
+        color2 = '255 255 255';
+        icon1= 'fa-sun';
+        icon2 = 'fa-moon';
+    }else{
+        modeTitle = 'Light';
+        color1 = '255 255 255';
+        color2 = '0 0 0';
+        icon1 = 'fa-moon';
+        icon2= 'fa-sun';
+    }
+
+    nav.style.backgroundColor = `rgb(${color1} / 50%)`;
+    textBox.style.backgroundColor = `rgb(${color2} / 50%)`;
+    toggleIcon.children[0].textContent = `${modeTitle} Mode`;
+    toggleIcon.children[1].classList.replace(`${icon1}`,`${icon2}`);
+  
+}
 
 //Switch Theme Dynamically
 function switchTheme(event){
+    var mode ='';
     if (event.target.checked) {
-        changeMode('dark');
+        mode ='dark';
     } else {
-        changeMode('light');
+        mode = 'light';
     }
+    changeMode(mode);
 }
 
 //Event Listeners
 toggleswitch.addEventListener('change', switchTheme);
+
+//Check Local Storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark'){
+        toggleswitch.checked=true;
+        changeMode('dark');
+    }
+}
